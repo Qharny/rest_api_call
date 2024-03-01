ae9a36a52c981930d91b81d07c5a8ae4a3a68721
Qharny <cannycc1020@gmail.com>
2024-03-01 03:29:05 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   lib/screen/home.dart
    	new file:   lib/screen/ipAdress.txt
    	deleted:    lib/screen/view.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,10 @@
+// ignore_for_file: use_key_in_widget_constructors, avoid_print
+
 import 'dart:convert';
-import 'dart:js';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:rest_api_call/color/color.dart';
-import 'package:rest_api_call/screen/view.dart';
+
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
@@ -16,6 +16,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<dynamic> users = [];
+
+  set isLoading(bool isLoading) {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,26 +34,22 @@ class _HomeState extends State<Home> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: ListView.builder(
-            itemCount: users.length,
-            itemBuilder: (context, index) {
-              final user = users[index];
-              final email = user['email'];
-              return ListTile(
-                title: Text(email),
-                //   title: Text('Name: ${users[index]['name']['first']}'),
-                //   subtitle: Text('Email: ${users[index]['email']}'),
-                //   leading: CircleAvatar(
-                //     backgroundImage:
-                //         NetworkImage(users[index]['picture']['thumbnail']),
-                //   ),
-              );
-            },
-          )),
-        ],
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          // final user = users[index];
+          // final email = user['email'];
+          // final name = user['name'];
+          return ListTile(
+            // title: Text(email),
+            title: Text('Name: ${users[index]['name']['first']}'),
+            subtitle: Text('Email: ${users[index]['email']}'),
+            leading: CircleAvatar(
+              backgroundImage:
+                  NetworkImage(users[index]['picture']['thumbnail']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: primaryColor,
@@ -62,61 +60,29 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Future<void> fetchUsers() async {
-    // creating variable to store url
-    const url = "https://randomuser.me/api/?results=10"; //  get random users
-    final link = Uri.parse(url);
-    final response = await http.get(link);
-    final body = response.body;
-    // decodes the json api code
-    final json = jsonDecode(body);
-    setState(() {
-      users = json('result');
-    });
-    print("Successful");
+  void fetchUsers() async {
+    print("Users called");
+    setState(() => isLoading = true);
+    try {
+      // creating variable to store url
+      const url = "https://randomuser.me/api/?results=100"; //  get random users
+      final link = Uri.parse(url);
+      final response = await http.get(link);
+      final body = response.body;
+      // decodes the json api code
+      final json = jsonDecode(body);
+      setState(() {
+        users = json['results'];
+      });
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() => isLoading = false);
+    }
+    print("Users called successful");
   }
 }
 
 
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:rest_api_call/color/color.dart';
-// import 'package:rest_api_call/screen/view.dart';
-
-// class home extends StatelessWidget {
-//   const home({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: whiteColor,
-//       appBar: AppBar(
-//         backgroundColor: primaryColor,
-//         title: const Text(
-//           'Rest API Call',
-//           style: TextStyle(
-//               color: whiteColor, fontSize: 30, fontWeight: FontWeight.w600),
-//         ),
-//         centerTitle: true,
-//       ),
-//       body: Column(
-//         children: [
-//           const viewList(),
-//           FloatingActionButton(
-            
-//             backgroundColor: primaryColor,
-//             child: const Icon(Icons.refresh, color: whiteColor),
-//             onPressed: () {
-//               print('Button Clicked');
-//             },
-//           ),
-//           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-//         ],
-//       ),
-//     );
-//   }
-// }
+// Get ip address
+// https://ipinfo.io/161.185.160.93/geo
