a05c924008a3faa07636a5d71df4e1684ffa32c1
Qharny <cannycc1020@gmail.com>
2024-03-01 00:59:22 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   lib/main.dart
    	modified:   lib/screen/home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:rest_api_call/color/color.dart';
 import 'package:rest_api_call/screen/view.dart';
 
-class home extends StatelessWidget {
-  const home({super.key});
+class Home extends StatelessWidget {
+  const Home({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +14,70 @@ class home extends StatelessWidget {
         title: const Text(
           'Rest API Call',
           style: TextStyle(
-              color: whiteColor, fontSize: 30, fontWeight: FontWeight.w600),
+            color: whiteColor,
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+          ),
         ),
         centerTitle: true,
       ),
-      body: Column(
+      body: const Column(
         children: [
-          const viewList(),
-          FloatingActionButton(
-            
-            backgroundColor: primaryColor,
-            child: const Icon(Icons.refresh, color: whiteColor),
-            onPressed: () {
-              print('Button Clicked');
-            },
-          )
+          Expanded(child: viewList()),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryColor,
+        child: const Icon(Icons.refresh, color: whiteColor),
+        onPressed: () {
+          // create a function to refresh the list
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:rest_api_call/color/color.dart';
+// import 'package:rest_api_call/screen/view.dart';
+
+// class home extends StatelessWidget {
+//   const home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: whiteColor,
+//       appBar: AppBar(
+//         backgroundColor: primaryColor,
+//         title: const Text(
+//           'Rest API Call',
+//           style: TextStyle(
+//               color: whiteColor, fontSize: 30, fontWeight: FontWeight.w600),
+//         ),
+//         centerTitle: true,
+//       ),
+//       body: Column(
+//         children: [
+//           const viewList(),
+//           FloatingActionButton(
+            
+//             backgroundColor: primaryColor,
+//             child: const Icon(Icons.refresh, color: whiteColor),
+//             onPressed: () {
+//               print('Button Clicked');
+//             },
+//           ),
+//           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+//         ],
+//       ),
+//     );
+//   }
+// }
